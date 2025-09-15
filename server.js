05e73e259bae7dbc8c9f601d3c79e2e779824afe
Jerry <jerry950627@gmail.com>
2025-09-16 01:06:22 +0800

    東吳大學資料科學系系學生會管理系統

--- server.js
+++ server.js
@@ -173,9 +173,9 @@ app.use((req, res, next) => {
     "default-src 'self'; " +
     "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.jsdelivr.net https://cdnjs.cloudflare.com; " +
     "style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net https://cdnjs.cloudflare.com https://fonts.googleapis.com; " +
-    "font-src 'self' https://fonts.gstatic.com https://cdnjs.cloudflare.com; " +
+    "font-src 'self' https://fonts.gstatic.com https://cdnjs.cloudflare.com https://cdn.jsdelivr.net; " +
     "img-src 'self' data: blob:; " +
-    "connect-src 'self'; " +
+    "connect-src 'self' https://cdn.jsdelivr.net https://cdnjs.cloudflare.com; " +
     "frame-ancestors 'none'; " +
     "base-uri 'self';"
   );
